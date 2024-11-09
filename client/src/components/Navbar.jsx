7260f4bfb20f5a8160519aab7a381976fe6b464b
Siwani jaiswal <siwani.jce20@sot.pdpu.ac.in>
2024-11-09 15:55:15 +0530

    added a logo in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import "../styles/Navbar.css";
+import NavLogo from "../assets/nav-logo.webp";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -13,44 +14,54 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">
-              {" "}
-              <h3> Products</h3>
-            </Link>
-          </li>
-          <li>
-            <Link to="/add">
-              <h3> Add Products</h3>
-            </Link>
-          </li>
-          <li>
-            <Link to="/update">
-              {" "}
-              <h3> Update Products</h3>
-            </Link>
-          </li>
-          <li>
-            <Link to="/profile">
-              {" "}
-              <h3>Profile </h3>
-            </Link>
-          </li>
-          <li>
-            {auth ? (
+        {auth ? (
+          <ul>
+            <div className="nav-logo">
+              <img src={NavLogo} width="60px" height="60px" />
+            </div>
+            <li>
+              <Link to="/">
+                {" "}
+                <h3> Products</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="/add">
+                <h3> Add Products</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="/update">
+                {" "}
+                <h3> Update Products</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile">
+                {" "}
+                <h3>Profile </h3>
+              </Link>
+            </li>
+            <li>
               <Link onClick={logOut} to="/signup">
                 {" "}
-                <h3> Logout</h3>
-              </Link>
-            ) : (
+                <h3> Logout ({JSON.parse(auth).message.name})</h3>
+              </Link>{" "}
+            </li>
+          </ul>
+        ) : (
+          <ul>
+            <div className="nav-logo">
+              <img src={NavLogo} width="60px" height="60px" />
+            </div>
+            <li>
               <Link to="/signup">
                 {" "}
                 <h3>SignUp</h3>
               </Link>
-            )}
-          </li>
-        </ul>
+            </li>
+          </ul>
+        )}
       </nav>
     </>
   );
